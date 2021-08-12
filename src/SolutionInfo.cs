bcf81bfb04500ae2d4d668c97ac5beb6733f02f6
Daniel Valadas <info@danielvaladas.com>
2021-08-12 15:35:46 -0400

    Reverted accidental deletion of solutioninfo.cs file

---
+++ src/SolutionInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Security;
+
+[assembly: AssemblyCompany("Shannon Deminick")]
+[assembly: AssemblyCopyright("Copyright © Shannon Deminick 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")] 
+
+
+[assembly: AssemblyVersion("1.9.10")]
+[assembly: AssemblyFileVersion("1.9.10")]
+[assembly: AssemblyInformationalVersion("1.9.10")]
